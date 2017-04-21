fbdb567ca5558bb394441134da112835d8f7bfa5
Jason Favrod <jason@epoquecorporation.com>
2017-04-21 12:58:05 -1000

    Added Doc Comments

--- SqlExpress.cs
+++ SqlExpress.cs
@@ -6,6 +6,7 @@ namespace SqlExpress
 {
     public class Db
     {
+        /** @var connectionString The connection string for the MSSQL Express DB **/
         private string connectionString = "";
 
 　
@@ -15,12 +16,32 @@ namespace SqlExpress
         }
 
 　
+        /**
+         * getConnString
+         * 
+         * Retrieves the connectionString.
+         * 
+         * @return string This object's connection string.
+         */
+
         public string getConnString()
         {
             return this.connectionString;
         }
 
 　
+        /**
+         * insert
+         * 
+         * Executes an insert style statement on the database. Can be
+         * of one of the following: CREATE TABLE, INSERT INTO, UPDATE,
+         * and DELETE.
+         * 
+         * @param string sql The sql statement to insert into the database.
+         * @return int The number of rows effected by the insert, or on
+         * CREATE TABLE or rollback -1.
+         */
+
         public int insert(string sql)
         {
             int result = 0;
